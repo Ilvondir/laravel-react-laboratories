9d5cb0600d3171bbe0ff50f469862551d8f70863
Ilvondir <komsa.m@o2.pl>
2023-08-01 15:03:13 +0200

    lesson about json resources

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\RegisterRequest;
 use App\Http\Requests\UpdateInfoRequest;
 use App\Http\Requests\UpdatePasswordRequest;
+use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -20,9 +21,10 @@ class AuthController extends Controller
             "last_name" => $request->input("last_name"),
             "email" => $request->input("email"),
             "password" => Hash::make($request->input("password")),
+            "role_id" => 1
         ]);
 
-        return response($user, Response::HTTP_CREATED);
+        return response(new UserResource($user), Response::HTTP_CREATED);
     }
 
     public function login(Request $request) {
@@ -45,7 +47,7 @@ class AuthController extends Controller
     }
 
     public function user(Request $request) {
-        return $request->user();
+        return new UserResource($request->user());
     }
 
     public function logout() {
@@ -60,7 +62,7 @@ class AuthController extends Controller
         /** @var User $user */
         $user = $request->user();
         $user->update($request->only("first_name", "last_name", "email"));
-        return response($user, Response::HTTP_ACCEPTED);
+        return response(new UserResource($user), Response::HTTP_ACCEPTED);
     }
 
     public function updatePassword(UpdatePasswordRequest $request)
@@ -70,6 +72,6 @@ class AuthController extends Controller
         $user->update([
             "password" => Hash::make($request->input("password"))
         ]);
-        return response($user, Response::HTTP_ACCEPTED);
+        return response(new UserResource($user), Response::HTTP_ACCEPTED);
     }
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\UserCreateRequest;
 use App\Http\Requests\UserUpdateRequest;
+use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -14,32 +15,32 @@ class UserController extends Controller
 
     public function index()
     {
-        return User::with("role")->paginate(5);
+        return UserResource::collection(User::paginate(5));
     }
 
 
     public function store(UserCreateRequest $request)
     {
         $user = User::create(
-            $request->only("first_name", "last_name", "email")
+            $request->only("first_name", "last_name", "email", "role_id")
             + ["password" => Hash::make("1234")]
         );
 
-        return response($user, Response::HTTP_CREATED);
+        return response(new UserResource($user), Response::HTTP_CREATED);
     }
 
 
     public function show(string $id)
     {
-        return User::with('role')->find($id);
+        return new UserResource(User::find($id));
     }
 
 
     public function update(UserUpdateRequest $request, string $id)
     {
         $user = User::find($id);
-        $user->update($request->only("first_name", "last_name", "email"));
-        return response($user, Response::HTTP_ACCEPTED);
+        $user->update($request->only("first_name", "last_name", "email", "role_id"));
+        return response(new UserResource($user), Response::HTTP_ACCEPTED);
     }
 
 
