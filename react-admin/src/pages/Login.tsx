9eb01f34f992fcc71359446083e4b6711f2501df
Ilvondir <komsa.m@o2.pl>
2023-08-09 10:12:52 +0200

    lesson about useState

---
+++ react-admin/src/pages/Login.tsx
@@ -0,0 +1,14 @@
+import React, {Component, useState} from 'react';
+
+const Login = () => {
+    const [count, setCount] = useState(0);
+
+    return (
+        <div className="container">
+            <h1>Count: {count}</h1>
+            <input type="number" onChange={e => setCount(parseInt(e.target.value)) } />
+        </div>
+    )
+}
+
+export default Login;
