37732d1655ded69f621765f55b805aa649904980
Ilvondir <komsa.m@o2.pl>
2023-08-13 16:28:06 +0200

    lesson about react models

--- react-admin/src/components/Nav.tsx
+++ react-admin/src/components/Nav.tsx
@@ -1,11 +1,10 @@
 import axios from "axios";
 import React, {useEffect, useState} from "react";
 import {Link, Navigate} from "react-router-dom";
+import {User} from "../models/user";
 
 const Nav = () => {
-    const [user, setUser] = useState({
-        first_name: ''
-    });
+    const [user, setUser] = useState(new User());
     const [redirect, setRedirect] = useState(false);
 
     useEffect(() => {
@@ -13,7 +12,13 @@ const Nav = () => {
             async () => {
                 try {
                     const response = await axios.get('user');
-                    setUser(response.data);
+                    setUser(new User(
+                        response.data.id,
+                        response.data.first_name,
+                        response.data.last_name,
+                        response.data.email,
+                    ));
+
                 } catch (e) {
                     setRedirect(true);
                 }
@@ -34,9 +39,9 @@ const Nav = () => {
     return (
         <nav className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
             <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Company name</a>
-
             <ul className="my-2 my-md-0 mr-md-3">
-                <Link to="/profile" className="p-2 text-white text-decoration-none">{user?.first_name}</Link>
+                <Link to="/profile"
+                      className="p-2 text-white text-decoration-none">{user?.first_name} {user?.last_name}</Link>
                 <Link to="/login" className="p-2 text-white text-decoration-none" onClick={logout}>Sign out</Link>
             </ul>
         </nav>
