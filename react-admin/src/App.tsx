c43b101f3e7c616269b3df967e48f18e7bdddcde
Ilvondir <komsa.m@o2.pl>
2023-08-17 16:00:26 +0200

    lesson about creating users

--- react-admin/src/App.tsx
+++ react-admin/src/App.tsx
@@ -5,6 +5,7 @@ import Dashboard from "./pages/Dashboard";
 import Users from "./pages/users/Users";
 import Register from "./pages/Register";
 import Login from './pages/Login';
+import UserCreate from "./pages/users/UserCreate";
 
 function App() {
     return (
@@ -13,9 +14,10 @@ function App() {
 
                 <Routes>
                     <Route path={'/'} element={<Dashboard/>}/>
-                    <Route path={'/users'} element={<Users/>}/>
                     <Route path={'/register'} element={<Register/>}/>
                     <Route path={'/login'} element={<Login/>}/>
+                    <Route path={'/users'} element={<Users/>}/>
+                    <Route path={'/users/create'} element={<UserCreate/>}/>
                 </Routes>
 
             </BrowserRouter>
